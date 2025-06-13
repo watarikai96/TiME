b6b0ca6840a96dad3077f3cd988ec4a0aec75fdc
Watari Kai <watarikai@outlook.com>
2025-06-13 19:03:44 +0900

    Refactored SessionStateDataStore.kt

--- TiME/app/src/main/java/com/time/android/datastore/SessionStateDataStore.kt
+++ TiME/app/src/main/java/com/time/android/datastore/SessionStateDataStore.kt
@@ -6,26 +6,15 @@ import androidx.datastore.dataStore
 import com.time.android.model.SessionStateSnapshot
 import kotlinx.coroutines.flow.firstOrNull
 
-
-
 val Context.sessionStateDataStore: DataStore<SessionStateSnapshot> by dataStore(
     fileName = "hyperfocus_session_state.json",
     serializer = SessionStateSerializer
 )
-
-
-
 class SessionStateDataStore(private val context: Context) {
-
-
-
     private val dataStore: DataStore<SessionStateSnapshot> = context.sessionStateDataStore
-
-
     suspend fun saveSessionState(state: SessionStateSnapshot) {
         dataStore.updateData { state }
     }
-
     suspend fun loadSessionState(): SessionStateSnapshot? {
         return try {
             dataStore.data.firstOrNull()
@@ -33,14 +22,9 @@ class SessionStateDataStore(private val context: Context) {
             null
         }
     }
-
     suspend fun clear() {
         dataStore.updateData {
             SessionStateSnapshot(emptyList(), 0, 0f, false, false, null)
         }
     }
-
-
-
 }
-
